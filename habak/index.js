cef19682a948cb38b131a33ee046a1d8ff4b099b
PC - Enzo Ferey <shoon96@gmail.com>
2018-06-02 12:16:54 +0200

    Habak skill.

---
+++ habak/index.js
@@ -0,0 +1,37 @@
+'use strict';
+const Alexa = require('alexa-sdk');
+
+const HELP_MESSAGE = 'You can ask me to say Habak';
+const HELP_REPROMPT = 'What can I help you with?';
+const STOP_MESSAGE = 'Goodbye!';
+
+const handlers = {
+    'LaunchRequest': function () {
+        this.emit('Habak');
+    },
+    'Habak': function () {
+        this.response.speak("Habak");
+        this.emit(':responseReady');
+    },
+    'AMAZON.HelpIntent': function () {
+        const speechOutput = HELP_MESSAGE;
+        const reprompt = HELP_REPROMPT;
+
+        this.response.speak(speechOutput).listen(reprompt);
+        this.emit(':responseReady');
+    },
+    'AMAZON.CancelIntent': function () {
+        this.response.speak(STOP_MESSAGE);
+        this.emit(':responseReady');
+    },
+    'AMAZON.StopIntent': function () {
+        this.response.speak(STOP_MESSAGE);
+        this.emit(':responseReady');
+    },
+};
+
+exports.handler = function (event, context, callback) {
+    const alexa = Alexa.handler(event, context, callback);
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
